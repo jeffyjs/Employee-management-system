3e5e429514a0d02c8ca5806ffe6528032b7c3b3e
Jefferson <jefferson@ruahtech.com.au>
2020-11-20 02:56:09 +0530

    Final testing

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { EmployeeInterface } from '../model/employee';
 
 @Injectable({
@@ -8,6 +7,15 @@ import { EmployeeInterface } from '../model/employee';
 export class EmployeeService {
 
   constructor( ) { }
+
+  getEmployeeData(){
+    const empdata = JSON.parse(localStorage.getItem('Employees'))
+      if(!empdata){
+        return [{name:"Empty", age:"Empty",department:"Empty", bloodGroup:"Empty", address:["Empty"], contactNumber:["Empty"]}]
+      }
+    return empdata;
+  }
+
 
   addEmployeeData(employeeData : EmployeeInterface){
     let allEmployees = [];
@@ -19,37 +27,47 @@ export class EmployeeService {
       allEmployees = [employeeData]
     }
     localStorage.setItem('Employees', JSON.stringify (allEmployees));
-    
   }
 
 
  
-   editEmployeeData(oldEmp){  let emps = JSON.parse(localStorage.getItem('Employees'));
-    for(let i = 0; i < emps.length; i++) {
-     if(emps[i].id == oldEmp.id) {
-       emps[i] = oldEmp;
-       console.log("from service",oldEmp)
+   editEmployeeData(updatedEmpData){  
+     
+    function removeEmptyValues(actual) {
+      let newArray = new Array();
+      for (let i = 0; i < actual.length; i++) {
+        if (actual[i]) {
+          newArray.push(actual[i]);
+        }
+      }
+      return newArray;
+    }
+    
+    let employeeData = JSON.parse(localStorage.getItem('Employees'));
+    for(let i = 0; i < employeeData.length; i++) {
+     if(employeeData[i].id == updatedEmpData.id) {
+       updatedEmpData.address = removeEmptyValues(updatedEmpData.address);
+       updatedEmpData.contactNumber = removeEmptyValues(updatedEmpData.contactNumber)
+       employeeData[i] = updatedEmpData;
      }
   }
-     localStorage.setItem('Employees', JSON.stringify(emps));
+     localStorage.setItem('Employees', JSON.stringify(employeeData));
   }
 
   
+  
 
   deleteEmployee(id) {
-    let emps = JSON.parse(localStorage.getItem('Employees'));
-    for(let i = 0; i < emps.length; i++) {
-     if(emps[i].id == id) {
-       emps.splice(i, 1);
+    let employeeData = JSON.parse(localStorage.getItem('Employees'));
+    for(let i = 0; i < employeeData.length; i++) {
+     if(employeeData[i].id == id) {
+       employeeData.splice(i, 1);
      }
   }
-     localStorage.setItem('Employees', JSON.stringify(emps));
+     localStorage.setItem('Employees', JSON.stringify(employeeData));
   }
 
-  getEmployeeData(){
-    const empdata = JSON.parse(localStorage.getItem('Employees'))
-    return empdata;
-  }
+ 
 
 }
 
